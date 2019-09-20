fbb6a33c60412523481ed939627637b4dd83d7c7
bilgeryahov <bayahov1@gmail.com>
2019-09-20 17:13:22 +0300

    Add a form to the Training component.

--- fitness-tracker/src/app/training/training.service.ts
+++ fitness-tracker/src/app/training/training.service.ts
@@ -1,6 +1,8 @@
 import { Exercise } from './exercise.model';
+import { Subject } from 'rxjs/Subject';
 
 export class TrainingService {
+    exerciseChanged = new Subject<Exercise>();
     private availableExercises: Exercise[] = [
         { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
         { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
@@ -15,5 +17,8 @@ export class TrainingService {
 
     startExercise(selectedId: string) {
         this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
+        this.exerciseChanged.next({
+            ...this.runningExercise
+        });
     }
 }
