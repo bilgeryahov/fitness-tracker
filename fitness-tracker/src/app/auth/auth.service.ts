ea2f5006db1ce1ad6f864be90268dac09a7b34d4
Bilger Yahov <bayahov1@gmail.com>
2019-10-03 14:46:31 +0300

    Re-organize the code.

--- fitness-tracker/src/app/auth/auth.service.ts
+++ fitness-tracker/src/app/auth/auth.service.ts
@@ -1,5 +1,4 @@
 import { Subject } from 'rxjs/Subject';
-import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
@@ -13,33 +12,36 @@ export class AuthService {
 
     constructor(private router: Router, private afAuth: AngularFireAuth, private trainingService: TrainingService) { }
 
+    initAuthListener() {
+        this.afAuth.authState.subscribe(user => {
+            if (user) {
+                this.isAuthenticated = true;
+                this.authChange.next(true);
+                this.router.navigate(['/training']);
+            } else {
+                this.trainingService.cancelSubsciptions();
+                this.isAuthenticated = false;
+                this.authChange.next(false);
+                this.router.navigate(['/login']);
+            }
+        });
+    }
+
     registerUser(authData: AuthData) {
         this.afAuth.auth.createUserWithEmailAndPassword(authData.email, authData.password)
-            .then(() => this.authSuccessfully())
             .catch((err) => console.error(err));
     }
 
     login(authData: AuthData) {
         this.afAuth.auth.signInWithEmailAndPassword(authData.email, authData.password)
-            .then(() => this.authSuccessfully())
             .catch((err) => console.error(err));
     }
 
     logout() {
-        this.trainingService.cancelSubsciptions();
         this.afAuth.auth.signOut();
-        this.isAuthenticated = false;
-        this.authChange.next(false);
-        this.router.navigate(['/login']);
     }
 
     isAuth() {
         return this.isAuthenticated;
-    }
-
-    private authSuccessfully() {
-        this.isAuthenticated = true;
-        this.authChange.next(true);
-        this.router.navigate(['/training']);
     }
 }
